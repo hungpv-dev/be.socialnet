47c747ddfac4cafc535174e91a30823d3372b21c
hungpv <phamvanhung.dev@gmail.com>
2024-07-25 06:34:34 +0700

    DONE

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('email')->unique();
+            $table->string('phone',20)->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('cover_avatar')->nullable();
+            $table->string('authentication')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('is_online')->default(false);
+            $table->boolean('is_active')->default(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
