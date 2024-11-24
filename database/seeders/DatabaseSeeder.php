65478f06e633b49b95315fe3795b03a308eed7e7
Dinh Quang Hien <dqhien2004@gmail.com>
2024-11-24 22:45:04 +0700

    update friend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,15 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
         // $this->call(ChatRoomSeeder::class);
         // ChatType::insert([['id' => 1,'name' => 'Chat riêng tư'],['id' => 2,'name' => 'Chat nhóm']]);
         // $this->call(MessageSeeder::class);
         $this->call(FriendRequestSeeder::class);
-        $this->call(FriendSeeder::class);
-        $this->call(BlockSeeder::class);
+        // $this->call(FriendSeeder::class);
+        // $this->call(BlockSeeder::class);
         // $this->call(UserStoriesSeeder::class);
-        $this->call(PostSeeder::class);
-        $this->call(NotificationSeeder::class);
+        // $this->call(PostSeeder::class);
+        // $this->call(NotificationSeeder::class);
     }
 }
