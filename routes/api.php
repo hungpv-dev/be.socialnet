0a5245ed11bf69429206b67c647b1a18eb884e2b
Dinh Quang Hien <dqhien2004@gmail.com>
2024-12-09 23:16:11 +0700

    update report manager - admin

--- routes/api.php
+++ routes/api.php
@@ -143,8 +143,8 @@ Route::middleware(['auth:api'])->group(function () {
     Route::resource('comments', CommentControler::class);
 });
 
-Route::prefix('admin')->group(function(){
+Route::prefix('admin')->group(function () {
     Route::resource('reports/type', Admin\ReportTypeController::class);
-    Route::resource('reports', Admin\ReportController::class);
+    Route::apiResource('reports', Admin\ReportController::class);
     Route::resource('users', Admin\UserController::class);
 });
